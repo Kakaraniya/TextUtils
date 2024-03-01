1c29a10fd1c9681d50b6cf3c7c37abfc3b92da2f
Nandini Kakaraniya <Nandini@Sajans-MacBook-Air.local>
2024-03-01 19:32:52 +0530

    second commit

--- textutils/urls.py
+++ textutils/urls.py
@@ -21,8 +21,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('analyze', views.analyze, name='analyze'),
-    path('ex1', views.ex1, name='ex1'),
+    path('analyze', views.analyze, name='analyze')
+
     # path('capitalizefirst', views.capfirst, name='capfirst'),
     # path('newlineremove', views.newlineremove, name='newlineremove'),
     # path('spaceremove', views.spaceremove, name='spaceremove'),
--- textutils/views.py
+++ textutils/views.py
@@ -2,23 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    #return HttpResponse('''<h1>hello user</h1>''')
-    return render(request, 'index.html')
-
-def ex1(request):
-    sites = ['''<h1>For Entertainment  </h1> <a href="https://www.youtube.com/"> Youtube Videos</a> ''',
-             '''<h1>For Interaction  </h1> <a href="https://www.facebook.com/"> Facebook</a> ''',
-             '''<h1>For Insight  </h1> <a href="https://www.ted.com/talks"> Ted Talks</a> ''',
-             '''<h1>For Internship  </h1> <a href="https://www.internshala.com">Internship</a> ''']
-    return HttpResponse((sites))
+    return render(request,"index.html")
 
 def analyze(request):
     # Get the text
-    djtext = request.GET.get('text', 'default')
-    removepunc=request.GET.get('removepunc','off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    djtext = request.POST.get('text', 'default')
+
+    removepunc=request.POST.get('removepunc','off')
+    fullcaps=request.POST.get('fullcaps','off')
+    newlineremover=request.POST.get('newlineremover','off')
+    extraspaceremover=request.POST.get('extraspaceremover','off')
 
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -27,54 +20,31 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-
-
-
-
-    elif (fullcaps == "on"):
-        analyzed = ""
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if fullcaps=="on":
+        analyzed=""
         for char in djtext:
-            analyzed = analyzed + char.upper()
+            analyzed=analyzed+char.upper()
         params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-
-
-
-    elif (extraspaceremover == "on"):
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if (extraspaceremover == "on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not (djtext[index] == " " and djtext[index + 1] == " "):
                 analyzed = analyzed + char
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
 
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
-
-
-
-    elif (newlineremover == "on"):
-        analyzed = ""
-        for char in djtext:
-            if char != "\n":
-                analyzed = analyzed + char
-
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
-
-
-
-    else:
-        return HttpResponse('Error')
-
-# def capfirst(request):
-#     return HttpResponse("capitalize first")
-#
-# def newlineremove(request):
-#     return HttpResponse("new line remover")
-#
-# def spaceremove(request):
-#     return HttpResponse("space remover")
-# def charcount(request):
-#     return HttpResponse("character count")
+    return render(request, 'analyze.html', params)
